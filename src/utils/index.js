241c3a7e5ec197392a5d24982bce262b8b64698c
mgstabrani <13519122@std.stei.itb.ac.id>
2022-03-12 20:53:07 +0700

    feature: get album songs detail

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,12 +3,14 @@ const mapAlbumDBToModel = ({
   id,
   name,
   year,
+  songs,
   created_at,
   updated_at,
 }) => ({
   id,
   name,
   year,
+  songs,
   createdAt: created_at,
   updatedAt: updated_at,
 });
