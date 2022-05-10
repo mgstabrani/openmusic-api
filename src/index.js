472b009a522a82fd2fd0a4c9c6470684e9cfa682
Mgs. Tabrani <63233752+mgstabrani@users.noreply.github.com>
2022-05-10 23:41:51 +0700

    feature: export (#14)

--- src/index.js
+++ src/index.js
@@ -35,6 +35,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// Exports
+const exportsAPI = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
@@ -122,6 +127,13 @@ const init = async () => {
         collaborationsService,
         playlistsService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: exportsAPI,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
