9b97d0d5fdfd80a8265b72e4e70cf6a1c64e7ed7
Mgs. Tabrani <63233752+mgstabrani@users.noreply.github.com>
2022-05-14 11:54:18 +0700

    feature-fix: upgrade server side cache code from version 3 to version 4 (#21)

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -3,48 +3,35 @@ const redis = require('redis');
 class CacheService {
   constructor() {
     this.client = redis.createClient({
-      host: process.env.REDIS_SERVER,
+      socket: {
+        host: process.env.REDIS_SERVER,
+      },
     });
+
     this.client.on('error', (error) => {
       // eslint-disable-next-line no-console
       console.error(error);
     });
+
+    this.client.connect();
   }
 
-  set(key, value, expirationInSecond = 1800) {
-    return new Promise((resolve, reject) => {
-      this.client.set(key, value, 'EX', expirationInSecond, (error, ok) => {
-        if (error) {
-          return reject(error);
-        }
-        return resolve(ok);
-      });
+  async set(key, value, expirationInSecond = 1800) {
+    await this.client.set(key, value, {
+      EX: expirationInSecond,
     });
   }
 
-  get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (error, reply) => {
-        if (error) {
-          return reject(error);
-        }
-        if (reply === null) {
-          return reject(new Error('Cache tidak ditemukan'));
-        }
-        return resolve(reply.toString());
-      });
-    });
+  async get(key) {
+    const result = await this.client.get(key);
+
+    if (result === null) throw new Error('Cache tidak ditemukan');
+
+    return result;
   }
 
   delete(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (error, count) => {
-        if (error) {
-          return reject(error);
-        }
-        return resolve(count);
-      });
-    });
+    return this.client.del(key);
   }
 }
 
