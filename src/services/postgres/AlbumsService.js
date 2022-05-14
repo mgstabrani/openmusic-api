68481897da9cedaf9d0f7b49683f1dcea3cf5941
Mgs. Tabrani <63233752+mgstabrani@users.noreply.github.com>
2022-05-14 12:34:06 +0700

    feature-fix: insert album query (#22)

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -17,7 +17,7 @@ class AlbumsService {
     const updatedAt = createdAt;
 
     const query = {
-      text: 'INSERT INTO albums VALUES($1, $2, $3, $4, $5) RETURNING id',
+      text: 'INSERT INTO albums VALUES($1, $2, $3, NULL, $4, $5) RETURNING id',
       values: [id, name, year, createdAt, updatedAt],
     };
 
