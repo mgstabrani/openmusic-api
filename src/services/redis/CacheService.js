5afca4867c2dd78f469f56d927837f82b168b552
Mgs. Tabrani <mgstabrani19@gmail.com>
2022-06-01 12:51:49 +0700

    feature-fix: redis connection

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -4,7 +4,9 @@ class CacheService {
   constructor() {
     this.client = redis.createClient({
       socket: {
-        host: process.env.REDIS_SERVER,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD,
       },
     });
 
